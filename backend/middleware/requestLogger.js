b94c2fd4a8e7efeb23e1ab33f3d5857647c6732e
Mr Arko Jana <arkojana45@gmail.com>
2026-05-30 19:22:26 +0530

    docs: add header comment to request logger middleware

--- backend/middleware/requestLogger.js
+++ backend/middleware/requestLogger.js
@@ -1,3 +1,5 @@
+// Request Logger Middleware - Logs incoming HTTP requests
+
 module.exports = (req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
